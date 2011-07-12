1126a1fd271cfa22ac8fa3b46c5640920c13244d
Matt <mattisbusiness@gmail.com>
2011-07-12 13:42:44 -0400

    Initial Commit

---
+++ R/describe.gam.poisson.R
@@ -0,0 +1,10 @@
+#' Describe the gam.poisson Zelig Model
+#' @param ... ignored parameters
+#' @return a list specifying author, title, etc. information
+#' @export
+describe.gam.poisson <- function(...) {
+  list(
+       authors = "",
+       text = ""
+       )
+}
---
+++ R/describe.gam.probit.R
@@ -0,0 +1,10 @@
+#' Describe the gam.probit Zelig Model
+#' @param ... ignored parameters
+#' @return a list specifying author, title, etc. information
+#' @export
+describe.gam.probit <- function(...) {
+  list(
+       authors = "",
+       text = ""
+       )
+}
